6db6ca1b3c1fd59466c906bc4d5f874eb0497b77
Thomas Widdows <thomas.widdows10@gmail.com>
2024-07-25 09:57:42 +0000

    register models in admin.py. rename items in models.py. Migration.

--- todo/models.py
+++ todo/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
-    name = mondels.CharField(max_length=50, null=False, blank=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):#
+        return self.name
